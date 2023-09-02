48750b35cab18a6d63ea0701e996e20a98fe0267
teaperr <143550106+teaperr@users.noreply.github.com>
2023-09-02 13:05:53 +0100

    0.0.1
    -added flint tool set (no textures or lang yet)
    -added a few bits of data from the old mod

--- src/main/java/net/teaper/aenarim/item/ModItems.java
+++ src/main/java/net/teaper/aenarim/item/ModItems.java
@@ -1,19 +1,35 @@
 package net.teaper.aenarim.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.teaper.aenarim.Aenarim;
+import net.teaper.aenarim.item.custom.ModHoeItem;
 
 public class ModItems {
 
-    private static final Item FIBRE = registerItem("fibre",
+    //material type items start
+    public static final Item FIBRE = registerItem("fibre",
             new Item (new FabricItemSettings().group(ItemGroup.MATERIALS)));
-    private static final Item DRIED_FIBRE = registerItem("dried_fibre",
+    public static final Item DRIED_FIBRE = registerItem("dried_fibre",
             new Item (new FabricItemSettings().group(ItemGroup.MATERIALS)));
 
+    //tool items start
+    public static final Item FLINT_KNIFE = registerItem("flint_knife",
+            new SwordItem(ModToolMaterial.FLINT, 1, 1.8f, new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item FLINT_HATCHET = registerItem("flint_hatchet",
+            new AxeItem(ModToolMaterial.FLINT, 2, 1f, new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item FLINT_PICKAXE = registerItem("flint_pickaxe",
+            new PickaxeItem(ModToolMaterial.FLINT, 1, 1f, new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item FLINT_SHOVEL = registerItem("flint_shovel",
+            new ShovelItem(ModToolMaterial.FLINT, 0.5F, 1.5f, new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item FLINT_HOE = registerItem("flint_hoe",
+            new ModHoeItem(ModToolMaterial.FLINT, 0, 2f, new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+    //adv items start
+
+    //adv items end
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Aenarim.MOD_ID, name), item);
     }
