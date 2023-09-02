f3d22b4cc90aa9fd37436729827218c119a4b986
teaperr <143550106+teaperr@users.noreply.github.com>
2023-09-02 10:15:42 +0100

    0.0.1
    -added fibre item
    -added dried fibre item

--- src/main/java/net/teaper/aenarim/Aenarim.java
+++ src/main/java/net/teaper/aenarim/Aenarim.java
@@ -1,6 +1,7 @@
 package net.teaper.aenarim;
 
 import net.fabricmc.api.ModInitializer;
+import net.teaper.aenarim.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,7 +9,11 @@ public class Aenarim implements ModInitializer {
 	public static final String MOD_ID = "aenarim";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
+
 	}
 }
--- src/main/java/net/teaper/aenarim/item/ModItems.java
+++ src/main/java/net/teaper/aenarim/item/ModItems.java
@@ -1,10 +1,25 @@
 package net.teaper.aenarim.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import net.teaper.aenarim.Aenarim;
 
 public class ModItems {
 
+    private static final Item FIBRE = registerItem("fibre",
+            new Item (new FabricItemSettings().group(ItemGroup.MATERIALS)));
+    private static final Item DRIED_FIBRE = registerItem("dried_fibre",
+            new Item (new FabricItemSettings().group(ItemGroup.MATERIALS)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(Aenarim.MOD_ID, name), item);
+    }
+
     public static void registerModItems() {
         Aenarim.LOGGER.debug("Registering Mod Items for " + Aenarim.MOD_ID);
+
     }
 }
